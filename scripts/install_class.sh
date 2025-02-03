8a4a1d2c139ba8d51520f9843ade41d3dfbd7f23
Vasilii Pustovoit <vpustovoit@cita.utoronto.ca>
2025-02-03 18:50:13 -0500

    Added CLASS installation script

---
+++ scripts/install_class.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# Catch exceptions
+if [[ "$INTERFACE_DIR" == "" ]]; then
+	echo "Please, set INTERFACE_DIR environment variable"
+	exit 1
+fi
+
+# Python venv setup
+python -m venv env
+source ./env/bin/activate
+pip install setuptool cython numpy scipy matplotlib
+
+# Install CLASS: F90 interface
+git clone https://github.com/lesgourg/class_public.git
+cd class_public
+git submodule update --init --recursive  # Critical for HyRec/Recfast data
+make -j4 CLASS_DIR=$INTERFACE_DIR/class_public
+
+# Set critical environment variables
+export CLASS_INCLUDE_DIR="/gpfs/fs0/scratch/m/murray/vasissua/PeakPatch/pp-music-interface/class_public/include"
+export CLASS_LIB="/gpfs/fs0/scratch/m/murray/vasissua/PeakPatch/pp-music-interface/class_public/lib"
+export LD_LIBRARY_PATH="${CLASS_LIB}:${LD_LIBRARY_PATH}"
+
+# Install CLASS: python interface
+cd python
+# Install with explicit library paths
+CFLAGS="-I${CLASS_INCLUDE_DIR} -L${CLASS_LIB}" \
+		LDFLAGS="-L${CLASS_LIB}" \
+		pip install -e .
